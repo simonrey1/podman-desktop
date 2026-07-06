aa856e1a01a20c811bc2f0dd2601a78a4c0cd4f2
Simon Rey <51708585+simonrey1@users.noreply.github.com>
2026-07-06 15:09:02 +0200

    feat: display dialog demo
    Signed-off-by: Simon Rey <51708585+simonrey1@users.noreply.github.com>

--- packages/renderer/src/lib/onboarding/wizard/OnboardingWelcomeSetupModePage.svelte
+++ packages/renderer/src/lib/onboarding/wizard/OnboardingWelcomeSetupModePage.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { Button, Link } from '@podman-desktop/ui-svelte';
 
+import OnboardingSkipConfirmDialog from './OnboardingSkipConfirmDialog.svelte';
 import OnboardingWelcomeSetupMode from './OnboardingWelcomeSetupMode.svelte';
 import OnboardingWelcomeTelemetry from './OnboardingWelcomeTelemetry.svelte';
 import OnboardingWizardShell from './OnboardingWizardShell.svelte';
@@ -8,6 +9,7 @@ import { ONBOARDING_WIZARD_DEFAULT_STEPS } from './OnboardingWizardSteps.constan
 import OnboardingWizardSteps from './OnboardingWizardSteps.svelte';
 
 const extensionId = 'podman';
+let showSkipDialog = $state(false);
 </script>
 
 <div class="h-full w-full">
@@ -28,9 +30,15 @@ const extensionId = 'podman';
 
     {#snippet footer()}
       <div class="flex items-center justify-between">
-        <Link>Skip entire setup</Link>
+        <Link onclick={(): void => { showSkipDialog = true; }}>Skip entire setup</Link>
         <Button>Start setup</Button>
       </div>
     {/snippet}
   </OnboardingWizardShell>
 </div>
+
+{#if showSkipDialog}
+  <OnboardingSkipConfirmDialog
+    onclose={(): void => { showSkipDialog = false; }} 
+    onskip={(): void => { showSkipDialog = false; }}/>
+{/if}
